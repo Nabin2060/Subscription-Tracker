bada6507ca484c16a5da518836332e6b965cd58c
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-16 21:37:12 +0545

    update the index file setup middleware and import new package cookie-parser,express default middleware

--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import config from './config/config.js';
 import connectDB from './config/db.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
 const PORT = config.PORT;
-connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+// routes
 // app.use('/api/v1/auth',)
+//app.use('/api/v1/users',)
+//app.use('/api/v1/subscriptions',)
 
+// error middleware
+app.use(errorMiddleware);
+
+// test route
 app.get('/', (req, res) => {
     res.send('api running');
 })
+// connect to database
+connectDB();
 
-
+// start the server
 app.listen(PORT, () => {
     console.log(`server is runnning on ${PORT}`);
 })
