c4d5f24da19381707996e8e7aff6c1c35b135474
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-18 21:37:13 +0545

    update the some env var

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,8 @@ const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 
 
 
