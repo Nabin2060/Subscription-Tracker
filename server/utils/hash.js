2aa27e48eada7622f512833d93e29408fcee7176
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-18 21:36:20 +0545

    complete a logic hash password and veify verify password using bcryptjs

---
+++ server/utils/hash.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+import config from '../config/config';
+
+
+export const hashPassword = async (password) => {
+    try {
+        const salt = config.SALT_ROUNDS;
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    } catch (err) {
+        throw new Error("Password hashing failed:)" + err.message);
+    }
+}
+
+// compare password
+export const comparePassword = async (password, hashedPassword) => {
+    try {
+        return await bcrypt.compare(password, hashedPassword);
+    } catch (err) {
+        throw new Error("Password comparison failed: " + err.message);
+    }
+}
