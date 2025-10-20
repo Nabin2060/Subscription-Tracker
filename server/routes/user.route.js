4d90a6320d393f1da478cee70477c225feb3b076
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-20 23:56:04 +0545

    to fix the route with access

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllUsers, getUser } from "../controllers/user.controller.js";
+import { authenticate, authorize } from "../middlewares/auth.middleware.js";
 
 export const userRouter = Router();
 
-userRouter.get('/allUsers', getAllUsers);
-userRouter.get('/allUsers/:id', getUser);
+userRouter.get('/allUsers', authenticate, authorize('admin'), getAllUsers);
+userRouter.get('/allUsers/:id', authenticate, getUser);
 
 
 export default userRouter;
