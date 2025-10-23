72d942b52ca639e7c95402671748c457d7767c2e
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-23 12:13:42 +0545

    new workflow logic add subscription controller

--- server/controllers/subscription.controller.js
+++ server/controllers/subscription.controller.js
@@ -1,4 +1,7 @@
+import config from "../config/config.js";
+import { workFlowClient } from "../config/upstash.js";
 import subscriptionModel from "../models/subscription.model.js";
+
 
 
 export const createSubscription = async (req, res) => {
@@ -7,11 +10,45 @@ export const createSubscription = async (req, res) => {
             ...req.body,
             user: req.user._id,
         });
+
+        const { workflowRunId } = await workFlowClient.trigger({
+            url: `${config.SERVER}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription._id,
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            retries: 0,
+        });
         res.status(201).json({
             success: true,
             message: "Subscription created successfully",
-            data: subscription,
+            data: {
+                subscription,
+                workflowRunId,
+            }
         })
+    } catch (err) {
+        res.status(500).json({ message: "Server Error:" + err.message });
+    }
+};
+
+export const getUserSubscriptions = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (req.user._id.toString() !== id.toString()) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access",
+            })
+        }
+        const subscriptions = await subscriptionModel.find({ user: id });
+        res.status(200).json({
+            success: true,
+            message: "User subscriptions fetched successfully",
+            data: subscriptions,
+        });
     } catch (err) {
         res.status(500).json({ message: "Server Error:" + err.message });
     }
